66887612e2b0b21fae23ca407df87cbc994415ae
EthsMC <69118918+EthsMC@users.noreply.github.com>
2021-04-12 23:54:20 -0400

    Update LunarBoard.java

--- eBoard/src/me/eths/eboard/Util/LunarBoard.java
+++ eBoard/src/me/eths/eboard/Util/LunarBoard.java
@@ -26,43 +26,43 @@ public class LunarBoard {
         }
 	}
 	
-    public static FastBoard getBoard(Player player) { 
-    	return boards.get(player.getUniqueId());
-    }
+	public static FastBoard getBoard(Player player) { 
+		return boards.get(player.getUniqueId());
+	}
     
-    public static boolean boardExists(Player player) {
-    	FastBoard exists = getBoard(player);
-    	if (exists != null) {
-    		return false;
-    	}
-    	return true;
-    }
+	public static boolean boardExists(Player player) {
+		FastBoard exists = getBoard(player);
+		if (exists != null) {
+			return false;
+			}
+		return true;
+	}
     
-    public static void updateBoardTitle(Player player, String title) {
-    	FastBoard board = getBoard(player);
-        board.updateTitle(title);
-    }
-    
+	public static void updateBoardTitle(Player player, String title) {
+		FastBoard board = getBoard(player);
+		board.updateTitle(title);
+	}
+	
 	public static void updateBoard(Player player, List<String> lines) {
 		FastBoard board = getBoard(player);
 		board.updateLines(lines);
-    }
+	}	
 	
 	public static void updateBoardLine(Player player, String line, Integer id) {
 		FastBoard board = getBoard(player);
 		board.updateLine(id, line);
-    }
+	}
 	
 	public static void deleteBoardLine(Player player, Integer id) {
 		FastBoard board = getBoard(player);
 		board.removeLine(id);
-    }
+	}
 	
 	public static void deleteAllLines(Player player) {
 		FastBoard board = getBoard(player);
 		board.updateLines("");
 		board.updateTitle("");
 		board.removeLine(0);
-    }
+	}
 	
 }
